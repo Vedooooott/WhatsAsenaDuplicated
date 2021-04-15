821a3fb135572663445c25a952b588bf8370ef82
Vedooooott <79120472+Vedooooott@users.noreply.github.com>
2021-04-15 03:44:52 +0300

    ..

--- plugins/admin.js
+++ plugins/admin.js
@@ -23,34 +23,24 @@ async function checkImAdmin(message, user = message.client.user.jid) {
     return sonuc.includes(true);
 }
 
-Asena.addCommand({pattern: 'ban ?(.*)', fromMe: true, onlyGroup: true, desc: Lang.BAN_DESC}, (async (message, match) => {  
+Asena.addCommand({pattern: 'ban1 ?(.*)', fromMe: true, onlyGroup: true, desc: Lang.BAN_DESC}, (async (message, match) => {  
     var im = await checkImAdmin(message);
     if (!im) return await message.client.sendMessage(message.jid,Lang.IM_NOT_ADMIN,MessageType.text);
 
     if (Config.BANMSG == 'default') {
         if (message.reply_message !== false) {
-            await message.client.sendMessage(message.jid,'@' + message.reply_message.data.participant.split('@')[0] + '```, ' + Lang.BANNED + '```', MessageType.text, {contextInfo: {mentionedJid: [message.reply_message.data.participant]}});
+            await message.client.sendMessage(
+                message.jid, 
+                fs.readFileSync("/root/WhatsAsenaDuplicated/media/gif/giphy.gif"),
+                MessageType.video, 
+                { mimetype: Mimetype.gif, caption: "FelixBlack Tarafından Banlandın hahaha!" }
+            )
+            await message.client.sendMessage(message.jid,'```zavallı``` ' +'@' + message.reply_message.data.participant.split("@")[0] + ' 😈', MessageType.text, {contextInfo: {mentionedJid: [message.reply_message.data.participant]}});
             await message.client.groupRemove(message.jid, [message.reply_message.data.participant]);
         } else if (message.reply_message === false && message.mention !== false) {
             var etiketler = '';
             message.mention.map(async (user) => {
-                etiketler += '@' + user.split('@')[0] + ',';
-            });
-
-            await message.client.sendMessage(message.jid,etiketler + '```, ' + Lang.BANNED + '```', MessageType.text, {contextInfo: {mentionedJid: message.mention}});
-            await message.client.groupRemove(message.jid, message.mention);
-        } else {
-            return await message.client.sendMessage(message.jid,Lang.GIVE_ME_USER,MessageType.text);
-        }
-    }
-    else {
-        if (message.reply_message !== false) {
-            await message.client.sendMessage(message.jid,'@' + message.reply_message.data.participant.split('@')[0] + Config.BANMSG, MessageType.text, {contextInfo: {mentionedJid: [message.reply_message.data.participant]}});
-            await message.client.groupRemove(message.jid, [message.reply_message.data.participant]);
-        } else if (message.reply_message === false && message.mention !== false) {
-            var etiketler = '';
-            message.mention.map(async (user) => {
-                etiketler += '@' + user.split('@')[0] + ',';
+                etiketler += '@' + user.split("@")[0];
             });
 
             await message.client.sendMessage(message.jid,etiketler + Config.BANMSG, MessageType.text, {contextInfo: {mentionedJid: message.mention}});
